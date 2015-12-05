4ae0527c6e66a1d72fd0183163af46fcf981090e
Samuel Bowman <samuelpbowman@gmail.com>
2015-12-05 13:48:57 -0500

    added bootstrap and minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ LoginApp::Application.routes.draw do
   resources :users, :user_sessions
   match 'login' => 'user_sessions#new', :as => :login, via: [:get, :post]
   match 'logout' => 'user_sessions#destroy', :as => :logout, via: [:get, :post]
+  root :to => redirect("users")
 end
