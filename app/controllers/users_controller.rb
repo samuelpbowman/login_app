82617aa3869d71d347730b146b91beb6d990d729
Samuel Bowman <samuelpbowman@gmail.com>
2015-12-05 03:01:03 -0500

    working on resolving forbiddenattributeerror

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,15 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(params[:user])
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to(:users, :notice => 'Registration successfull.') }
+        format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205071132) do
+ActiveRecord::Schema.define(version: 20151205072250) do
+
+  create_table "user_sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
