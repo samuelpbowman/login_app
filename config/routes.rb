239a89fd427e9c4a338bdae6f2536ea99ebc5c79
Samuel Bowman <samuelpbowman@gmail.com>
2015-12-06 23:19:20 -0500

    pictures look nicer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LoginApp::Application.routes.draw do
+  get 'welcome/index'
+
   resources :users, :user_sessions
   match 'login' => 'user_sessions#new', :as => :login, via: [:get, :post]
   match 'logout' => 'user_sessions#destroy', :as => :logout, via: [:get, :post]
